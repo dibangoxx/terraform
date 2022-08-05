8c1af60682dc2348b7552d52b196be6d3667f137
dibangoxx <99225889+dibangoxx@users.noreply.github.com>
2022-08-04 19:06:28 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
-    region = "us-west-2"
+    bucket = "codex1-bucket"
+    key    = "didcx1.tfstate"
+    region = "us-east-1"
     profile = "dibango"
 dynamodb_table= "firstprojecttable"
    }
