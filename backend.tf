a4dc043f093f733f8d349a1e4ad59452b79afbd4
dibangoxx <99225889+dibangoxx@users.noreply.github.com>
2022-08-04 19:42:10 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
- terraform {
-   backend "s3" {
-    bucket = "codex1-bucket"
-    key    = "didcx1.tfstate"
-    region = "us-east-1"
-    profile = "dibango"
-dynamodb_table= "firstprojecttable"
-   }
- }
+#  terraform {
+#    backend "s3" {
+#     bucket = "codex1-bucket"
+#     key    = "didcx1.tfstate"
+#     region = "us-east-1"
+#     profile = "dibango"
+# dynamodb_table= "firstprojecttable"
+#    }
+#  }
 
